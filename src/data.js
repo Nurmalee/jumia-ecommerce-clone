614b56762258f5bc93dcd188fa1dd70c5f49f2ea
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-07 10:39:05 +0100

    populated banner shuffle array with image links for shuffle effect

--- src/data.js
+++ src/data.js
@@ -94,6 +94,13 @@ export const sideNavMenu = [
 ]
 
 export const bannerShuffle = [
-    "https://ng.jumia.is/cms/Homepage/2021/w14/Artboard_1_copy_2.jpg",
+    "https://ng.jumia.is/cms/FOODFEST/11S1424x768__-(1).jpg", //JUMIA FOOD
+    "https://ng.jumia.is/cms/FOODFEST/11JFF_KrispyKreame_1424x768-min.jpg", //JUMIA FOOD FEST
+    "https://ng.jumia.is/cms/Homepage/2021/w14/Super-(1).jpg", //SUPERMARKET
+    "https://ng.jumia.is/cms/Homepage/2021/w14/Artboard_1_copy.jpg", //TREND WOMEN
+    "https://ng.jumia.is/cms/Homepage/2021/w14/Artboard_1_copy_2.jpg", //TREND MEN
+    "https://ng.jumia.is/cms/Homepage/2021/w13/Mobile_Desktop.jpg", //DONT MISS A CALL
+    "https://ng.jumia.is/cms/Homepage/2021/w13/26DB5C4C-B436-40B3-82DF-6D894E9A50B8.jpeg", //FLASH SALES
+    "https://ng.jumia.is/cms/Homepage/2021/w14/Artboard_1.jpg", //TREND SPORTING
 
 ]
