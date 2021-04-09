5234fa4cf1af6a5c695e0db532467274c9421572
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-09 11:43:51 +0100

    improved the dynamism for reusability

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
+import { Star } from '@material-ui/icons';
 
-const SingleProduct = ({name, desc, image, current_price, old_price, eligibility_statement}) => {
+
+const SingleProduct = ({name, status, desc, image, current_price, percentReduct, eligibility_statement}) => {
+
     return (
         <Product>
 
@@ -9,10 +12,16 @@ const SingleProduct = ({name, desc, image, current_price, old_price, eligibility
                 <img src={image} alt={name} />
             </ProductImage>
 
+            {status && <p style={{backgroundColor: status.abroad ? "blue" : "brown"}}> {status.state} </p>}
+
             <ProductDetails>
-                <p> <b>{name.toUpperCase()}</b> - {desc} </p>
-                <h3> # {current_price} </h3>
-                <h4> # {old_price} </h4>
+                <p> <b>{name ? name.toUpperCase() : null}</b> {desc} </p>
+                <h3> &#8358; {current_price} </h3>
+                <h4> &#8358; <span>{Number(current_price) + parseInt(Number(current_price) * Number(percentReduct))}</span> <span> -{parseInt(percentReduct * 100)}% </span> </h4>
+
+                <div>
+                    <Star style={{fontSize: "16px"}}/> <Star style={{fontSize: "16px"}}/> <Star style={{fontSize: "16px"}}/> <Star style={{fontSize: "16px"}}/>
+                </div>
             </ProductDetails>
 
             <ExtraDetails>
@@ -32,10 +41,24 @@ const Product = styled.div`
 /* border: 1px solid blue; */
     width: 20%;
     background-color: white;
+    color: #555;
     padding: 7px;
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    /* margin-right: 7px; */
     border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    /* align-self: center; */
+
+    > p {
+        /* background-color: status.abroad ? blue : red; */
+        color: white;
+        margin: 5px 0;
+        border-radius: 3px;
+        padding: 3px;
+        font-size: 11px;
+        width: max-content;
+    }
 
     &:last-of-type {
         margin-right: 0;
@@ -91,15 +114,27 @@ const ProductDetails = styled.div`
     font-weight: 100;
 
     > h3 {
-        font-size: 15px;
+        font-size: 14px;
         padding-top: 10px;
     }
 
     > h4 {
-        font-size: 13px;
-        text-decoration: line-through;
+        font-size: 12px;
         color: #777;
         padding-bottom: 10px;
+        margin-top: 5px;
+
+        > span {
+            text-decoration: line-through;
+
+            &:nth-of-type(2) {
+                color: darkorange;
+                text-decoration: none;
+                background-color: #feefe2;
+                padding: 5px 2px;
+                margin-left: 5px;
+            }
+        }
     }
 
     > p {
@@ -107,6 +142,11 @@ const ProductDetails = styled.div`
         cursor: pointer;
         color: #333;
         text-transform: capitalize;
+    }
+
+    > div {
+        display: flex;
+        color: darkorange;
     }
 `
 
