47afda47c9bbe0fff38693a0be3204939beb2209
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-09 11:44:31 +0100

    componet inclusion and AdBanner styling

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,12 @@ import styled from 'styled-components'
 import Banner from './Banner'
 import ExtendedLinks from './ExtendedLinks'
 import SideNav from './SideNav'
-import TopSelling from './TopSelling'
+import TopProducts from './TopProducts'
+
+import { topProducts, topDeals }  from '../data'
 
 const Body = () => {
+
     return (
         <BodyContainer>
 
@@ -15,8 +18,19 @@ const Body = () => {
            
             <ExtendedLinks />
 
-            <TopSelling />
+            <TopProducts colorCode={{bg: "white", text: "black" }} categoryHeading="Top selling items" productArray={topProducts} />
+
+            <AdBanner>
+                <img src="https://ng.jumia.is/cms/Homepage/2021/w14/11CB-1152x252.jpg" alt="adveert"/>
+            </AdBanner>
+
+            <TopProducts colorCode={{bg: "darkred", text: "white" }} categoryHeading="Deals of the day | starting from #1,000" productArray={topDeals}/>
             
+            <AdBanner>
+                <img src="https://ng.jumia.is/cms/Homepage/2021/w07/Smartwatches_.jpg" alt="adveert"/>
+                <img src=" https://ng.jumia.is/cms/Homepage/2021/w14/MFLmen_s_shoes.jpg" alt="adveert"/>
+            </AdBanner>
+
         </BodyContainer>
     )
 }
@@ -35,5 +49,33 @@ const BodyContainer = styled.main`
 
     @media screen and (max-width: 1200px) {
         width: 1000px;
+    }
+`
+
+const AdBanner = styled.div`
+    margin: 10px auto;
+    padding: 10px;
+    background-color: white;
+    text-align: center;
+    height: 270px;
+    width: 100%;
+    border-radius: 5px;
+    display: flex;
+
+    > img {
+        object-fit: fill;
+        height: 100%;
+        width: 100%;
+        border-radius: 5px;
+        cursor: pointer;
+
+        &:nth-of-type(2) {
+            margin-left: 10px;
+        }
+
+        &:hover {
+            transform: scale(1.005);
+            box-shadow: 0 0 10px #bbb;
+        }
     }
 `
