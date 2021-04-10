620cf6acf7999bc58e955201e13508d7a0c68f51
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-10 09:59:37 +0100

    Wrapped context provider around app compnents

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 import App from './App'
+import { AppState } from "./context" 
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <AppState>
+            <App />
+        </AppState>
     </React.StrictMode>, 
     document.getElementById("root")
 )
