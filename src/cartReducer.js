4baaba0f8fbe264d3b83690e3fc39662b5f98d9f
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-11 22:44:33 +0100

    Updated the reducer for add item and update item quantity functionalities

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -3,15 +3,24 @@ import ACTION from "./actions"
 const cartReducer = (state, action) => {
     switch(action.type){
         case ACTION.ADD_ITEM:
+
+        const itemAlreadyInCart = state.cart.find(item => item.id === action.payload.id ? true : false)
+
         return {
             ...state,
-            cart: [...state.cart, action.payload]
+            cart: itemAlreadyInCart ? state.cart.map(item => item.id === action.payload.id ? {...item, qty: item.qty + 1} : item) : [...state.cart, {...action.payload, qty: 1}]
         }
 
         case ACTION.REMOVE_ITEM:
         return {
             ...state,
             cart: state.cart.filter(item => item.id !== action.payload)
+        }
+
+        case ACTION.UPDATE_ITEM_QAUNTITY:
+        return {
+            ...state,
+            cart: state.cart.map(item => item.id === action.payload.id ? {...item, qty: +action.payload.qty} : item)
         }
 
         case ACTION.CLEAR_CART:
