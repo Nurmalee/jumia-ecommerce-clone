a64dda30c322936423a108fb508b846f038cb6ae
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-11 22:41:56 +0100

    Gave each product a unique id

--- src/data.js
+++ src/data.js
@@ -91,6 +91,7 @@ export const bannerShuffle = [
 
 export const topProducts = [
     {
+        id: 1,
         name: "samsung",
         desc: 'Samsung Galaxy A12 - 6.5" 48MP Camera, 4/64GB Memory, 5000maH Battery, Fingerprint, 4G LTE - BLACK',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/21/886217/1.jpg?9326",
@@ -99,6 +100,7 @@ export const topProducts = [
         eligibility_statement: "eligibe for free shipping with jumia express (lagos only) OR jumia prime"
     },
     {
+        id: 2,
         name: "iPhone",
         desc: 'Apple IPhone 12 Pro - 128GB, 6GB RAM, 6.1-Inch, 5G, (12MP+12MP+12MP), 2815mAh-Pacific Blue',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/22/211437/1.jpg?0129",
@@ -111,6 +113,7 @@ export const topProducts = [
         eligibility_statement: "eligibe for free shipping with jumia express (lagos only) OR jumia prime"
     },
     {
+        id: 3,
         name: "PS5",
         desc: 'Sony Playstation 5 825GB - Installed Games (fifa21, Gta5, Mk11,God Of War, Nfs...)',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/01/786617/1.jpg?3547",
@@ -123,6 +126,7 @@ export const topProducts = [
         eligibility_statement: "eligibe for free shipping with jumia express (lagos only) OR jumia prime"
     },
     {
+        id: 4,
         name: "dell",
         desc: 'DELL Inspiron 13 7000 Series 2-in-1 Touch/Backlit Keyboard Intel Core I5-8GB RAM-512GB SSD + 32GB Optane',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/24/906717/1.jpg?9306",
@@ -135,6 +139,7 @@ export const topProducts = [
         eligibility_statement: "eligibe for free shipping with jumia express (lagos only) OR jumia prime"
     },
     {
+        id: 5,
         name: "Megir",
         desc: 'Megir Man Watch Genuine Leather Band 3 Small Dials',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/45/838653/1.jpg?8237",
@@ -146,6 +151,7 @@ export const topProducts = [
 
 export const topDeals = [
     {
+        id: 6,
         desc: "Mama'S Pride Parboiled Rice 50kg",
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/28/054676/1.jpg?4277",
         current_price: "24990",
@@ -157,6 +163,7 @@ export const topDeals = [
         eligibility_statement: "Free Shipping on Jumia Express orders above N12,000 in Lagos"
     },
     {
+        id: 7,
         desc: 'QUALITY WOMEN 4 IN 1 SHOULDER HAND BAG, BLACK COLOUR',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/30/330207/1.jpg?5388",
         current_price: "6600",
@@ -164,6 +171,7 @@ export const topDeals = [
         eligibility_statement: "eligibe for free shipping with jumia express (lagos only) OR jumia prime"
     },
     {
+        id: 8,
         desc: '2 In 1 Walkabout Sneakers & Tstain Maintenance Kit-v2',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/25/508366/1.jpg?2077",
         current_price: "2740",
@@ -171,6 +179,7 @@ export const topDeals = [
         eligibility_statement: "Free Shipping on Jumia Express orders above N12,000 in Lagos"
     },
     {
+        id: 9,
         desc: 'Chrysolite Designs Lucky Numbers 6 IN 1 Casual Polo Bundle - Navyblue, Black, White, Grey, Wine, Red.',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/69/747454/1.jpg?9869",
         current_price: "11440",
@@ -178,6 +187,7 @@ export const topDeals = [
         eligibility_statement: "Free Shipping on Jumia Express orders above N12,000 in Lagos"
     },
     {
+        id: 10,
         desc: 'Silicone LED Digital Sports Electronic Wristwatch - Unisex',
         image: "https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/53/718375/1.jpg?0866",
         current_price: "1850",
