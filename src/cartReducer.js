4d2d9ad18772bb48d74dc31f5679f07c8a58176a
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-04-14 17:17:14 +0100

    Included item in cart explanatory expression

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -6,6 +6,21 @@ const cartReducer = (state, action) => {
 
         const itemAlreadyInCart = state.cart.find(item => item.id === action.payload.id ? true : false)
 
+        //IF CONFUING READ THROUGH THIS
+            /* 
+            if(itemAlreadyInCart){
+                return {
+                    ...state,
+                    cart: state.cart.map(item => item.id === action.payload.id ? {..item, qty: item.qty + 1})
+                }
+            } else {
+                return {
+                    ...state,
+                    cart: [...state.cart, {...action.payload, qty: 1}]
+                }
+            }
+            */
+
         return {
             ...state,
             cart: itemAlreadyInCart ? state.cart.map(item => item.id === action.payload.id ? {...item, qty: item.qty + 1} : item) : [...state.cart, {...action.payload, qty: 1}]
